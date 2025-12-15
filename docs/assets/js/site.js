df9118f1d8f9cafb6d93944cd57c08be1f64f1f9
socanc <jpwilson84@yahoo.com>
2025-12-14 19:39:17 -0500

    i made a comment!!!

--- docs/assets/js/site.js
+++ docs/assets/js/site.js
@@ -128,7 +128,7 @@ function DiscardTile(isAttackPossible){
         discardedTiles.unshift(tileToDiscard);
         return;
     }
-    HandleOutOfTiles();
+    HandleOutOfTiles();//wilsonwashere
     alert("You have ran of tiles to Discard => final flicker");
 }
 
